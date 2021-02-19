24213d59f3bc053b63167c79b847bc40d8ba9b7d
Callie Stump <callie@stu.mp>
2021-02-19 11:38:48 -0500

    good fail to see if keg gets correctly added to kegMasterList'

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,40 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    kegName: 'Quarter-Barrel',
+    kegBrand: 'Miller Lite',
+    kegPintQuantity: 124,
+    kegAlcoholContent: "25.6%",
+    kegPrice: "12.99",
+    id: 1
+  }
   test('Should return the default state if it doesnt regonize any action types passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   })
-})
+
+  test('Should successfully add a new keg data to masterKegList', () => {
+    const { kegName, kegBrand, kegPintQuantity, kegAlcoholContent, kegPrice, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      kegName,
+      kegBrand,
+      kegPintQuantity,
+      kegAlcoholContent,
+      kegPrice,
+      id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        kegName,
+        kegBrand,
+        kegPintQuantity,
+        kegAlcoholContent,
+        kegPrice,
+        id
+      }
+    })
+  })
+});
