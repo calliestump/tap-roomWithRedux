d250e6ac67a3dcbaf2b72ec16cb890a701167205
Callie Stump <callie@stu.mp>
2021-02-19 11:42:01 -0500

    pass test to see if keg gets correctly added to masterKegList

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
-}
+  const { kegName, kegBrand, kegPintQuantity, kegAlcoholContent, kegPrice, id } = action;
+  switch (action.type) {
+  case 'ADD_KEG':
+    return Object.assign({}, state, {
+      [id] : {
+        kegName,
+        kegBrand,
+        kegPintQuantity,
+        kegAlcoholContent,
+        kegPrice,
+        id
+      }
+    });
+    default:
+      return state;
+  }
+};
